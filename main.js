e8c85c5f079987e2b03fe176c9741c9ed669677d
Andrew Breckenridge <asbreckenridge@me.com>
2016-04-13 21:13:59 -0700

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+view Main {
+  <h1>Hello world!</h1>
+  <img src="https://mail-attachment.googleusercontent.com/attachment/u/1/?ui=2&ik=3b282328d9&attid=0.1&th=15402ed3007e3fc1&view=att&disp=inline&realattid=f_imu9club0&zw&saddbat=ANGjdJ_SqVTBbqqdRMBvH-kJHuhkwtOzOwMaAiZXkwMEAz1sIIEhU5ZS2iLgvYNWXcnn0z2OUt-Q2wa0cZubzqKen8fR26hS8mYLBER08tzJNViZs1S4Zcl2EoNENNhAXt9VafSTOOifeCWqLGq33yc_HP8kzCSfduzilnBJzYfMXxdir3gvnr-DcWuCi_KeQz9IIZeBKrPml4SgxusJbAGGPcl-diT7CEbi9E4uq8dzO23m5K7opZv4trtoDwRaQk2EzAk_0QwjsENW-woH4zpxH6BiCcwY2TtkGucnIiByhIPOi0UpBUyQsAifKY5FfLtQUzOQrjfPWNJj5mVDtu80ry_ZWRzkhsH683N-51zSxh3KSbjk8C8AJQ4y40spismXNlhWUT7k9gQEW3naBLXC_SzTARliEMb7lM8u6Qf8LEmTUjSWaSZu73PqQNxssKLIXZd6hjzQAfjAKqNqBrg9Mj0RKEpUC01vZ7uumUBYh3XS2oD_PL0pcsRio7_fxob45I7pIxEip8Jnmp0C2vG-P2yoE9B6ctiuDRMvFSYivvVrDelIZUfBJSYb1FVtL2o_Vw5pd_ar6MAzPX2uzNN20BM1TTWPI2dDBDSoLKbRrb1TNEtSyOPvO-0D8YRWqBMfM_6KsRhtg3wGYAKS" />
+
+  $ = {
+    color: '#000',
+    padding: 20
+  }
+}
